65ee698439e416a7e7fedb3f3fc7379cd695eaac
Alexei_korn <aykornjushenkov@gmail.com>
2021-12-09 21:08:43 +0300

    initial checkstyle

---
+++ src/test/java/ru/job4j/it/ArrayItTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.it;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class ArrayItTest {
+
+    @Test
+    public void whenMultiCallhasNextThenTrue() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        Assert.assertTrue(it.hasNext());
+        Assert.assertTrue(it.hasNext());
+    }
+
+    @Test
+    public void whenReadSequence() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        Assert.assertEquals((long) it.next(), 1L);
+        Assert.assertEquals((long) it.next(), 2L);
+        Assert.assertEquals((long) it.next(), 3L);
+    }
+
+}
