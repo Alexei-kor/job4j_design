1b8cda623fb2455bcc8261bec4b2213ae8b6ffb4
Alexei_korn <aykornjushenkov@gmail.com>
2021-12-18 11:20:49 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,87 @@
+package ru.job4j.list;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements List<T> {
+
+    private Object[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        modCount++;
+        if (size == container.length) {
+            grow();
+        }
+        container[size] = value;
+        size++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, container.length);
+        Object old = container[index];
+        container[index] = newValue;
+        return (T) old;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, container.length);
+        modCount++;
+        Object old = container[index];
+        System.arraycopy(container, index + 1, container, index, size - index);
+        container[size] = null;
+        size--;
+        return (T) old;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, container.length);
+        return (T) container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private int point = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return (T) container[point++];
+            }
+
+        };
+    }
+
+    public void grow() {
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+
+}
