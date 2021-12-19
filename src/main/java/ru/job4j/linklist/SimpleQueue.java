b1de006df554b3df543c07d11086fa6b5d295b78
Alexei_korn <aykornjushenkov@gmail.com>
2021-12-19 10:10:51 +0300

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/linklist/SimpleQueue.java
@@ -0,0 +1,16 @@
+package ru.job4j.linklist;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        T rsl = in.pop();
+        out.push(rsl);
+        return rsl;
+    }
+
+    public void push(T value) {
+        in.push(value);
+    }
+}
