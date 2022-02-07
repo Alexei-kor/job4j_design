b2ffb8e604d02eaa16ff1ce19ee740716da336d3
Alexei_korn <aykornjushenkov@gmail.com>
2022-02-07 22:28:56 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tree;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,11 +11,11 @@ public class SimpleTreeTest {
     @Test
     public void when6ElFindLastThen6() {
         Tree<Integer> tree = new SimpleTree<>(1);
-        tree.add(1, 2);
-        tree.add(1, 3);
-        tree.add(1, 4);
-        tree.add(4, 5);
-        tree.add(5, 6);
+        Assert.assertTrue(tree.add(1, 2));
+        Assert.assertTrue(tree.add(1, 3));
+        Assert.assertTrue(tree.add(1, 4));
+        Assert.assertTrue(tree.add(4, 5));
+        Assert.assertTrue(tree.add(5, 6));
         assertThat(
                 tree.findBy(6).isPresent(),
                 is(true)
@@ -40,6 +41,32 @@ public class SimpleTreeTest {
         tree.add(4, 5);
         tree.add(5, 6);
         assertFalse(tree.add(2, 6));
+    }
+
+    @Test
+    public void whenIsNotBinaryTree() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        Assert.assertTrue(tree.add(1, 2));
+        Assert.assertTrue(tree.add(1, 3));
+        Assert.assertTrue(tree.add(1, 4));
+        Assert.assertTrue(tree.add(4, 5));
+        Assert.assertTrue(tree.add(5, 6));
+        Assert.assertTrue(tree.add(6, 7));
+        Assert.assertTrue(tree.add(6, 8));
+        Assert.assertTrue(tree.add(6, 9));
+        Assert.assertFalse(tree.isBinary());
+    }
+
+    @Test
+    public void whenIsBinaryTree() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        Assert.assertTrue(tree.add(1, 2));
+        Assert.assertTrue(tree.add(1, 4));
+        Assert.assertTrue(tree.add(4, 5));
+        Assert.assertTrue(tree.add(5, 6));
+        Assert.assertTrue(tree.add(6, 7));
+        Assert.assertTrue(tree.add(6, 9));
+        Assert.assertTrue(tree.isBinary());
     }
 
 }
