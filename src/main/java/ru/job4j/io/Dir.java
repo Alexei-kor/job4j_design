65feb6585fbc8baa1cfd5ce68bb07b79f9271f9d
Alexei_korn <aykornjushenkov@gmail.com>
2022-03-21 21:31:14 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("./Data");
+        if (!file.exists()) {
+            new IllegalArgumentException(String.format("No exist %s", file.getAbsolutePath()));
+        }
+        if (!file.isDirectory()) {
+            new IllegalArgumentException(String.format(String.format("It'n directory %s", file.getAbsolutePath())));
+        }
+        System.out.println(String.format("size %s", file.getTotalSpace()));
+        Arrays.stream(file.listFiles())
+                .filter(e -> !e.isDirectory())
+                .map(e -> String.format("Name: '%s', size: %s", e.getName(), e.length()))
+                .forEach(System.out::println);
+    }
+}
