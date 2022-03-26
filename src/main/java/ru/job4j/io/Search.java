bb7ba859ff6d88ddc9763a42e19462fbd279b670
Alexei_korn <aykornjushenkov@gmail.com>
2022-03-26 14:11:52 +0300

    5. Валидация параметров запуска. [#246865 #279443] Отработка замечаний

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,44 +11,23 @@ import java.util.function.Predicate;
 
 public class Search {
 
-    private String head;
-    private String ext;
-
-    public Search(String head, String ext) {
-        this.head = head;
+    public static void main(String[] args) throws IOException {
+        checkParams(args);
+        String head = args[0];
+        String ext = args[1];
         if (!ext.startsWith(".")) {
             ext = "." + ext;
         }
-        this.ext = ext;
-    }
-
-    public String getHead() {
-        return head;
-    }
-
-    public String getExt() {
-        return ext;
-    }
-
-    public static void main(String[] args) throws IOException {
-        checkParams(args);
-        Search search = new Search(args[0], args[1]);
-        final String extF = search.getExt();
-        search.run(
-                Paths.get(search.getHead()),
-                p -> p.toFile().getName().endsWith(extF)
-                )
+        final String extF = ext;
+        search(Paths.get(head), p -> p.toFile().getName().endsWith(extF))
                 .forEach(System.out::println);
     }
 
     public static void checkParams(String[] args) {
-        if (args.length < 2) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Must be 2 required params");
         }
         String head = args[0];
-        if (head.isEmpty()) {
-            throw new IllegalArgumentException("Empty head catalog");
-        }
         Path path = Paths.get(head);
         if (!path.toFile().exists()) {
             throw new IllegalArgumentException(String.format("Сatalog '%s' does't exist", head));
@@ -63,7 +42,7 @@ public class Search {
         }
     }
 
-    public List<Path> run(Path root, Predicate<Path> condition) throws IOException {
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
