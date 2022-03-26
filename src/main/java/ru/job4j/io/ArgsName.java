42e6256e467cf115943c91c62dd0fa718914b716
Alexei_korn <aykornjushenkov@gmail.com>
2022-03-26 14:29:22 +0300

    5.1. Именованные аргументы [#295518 #279996] Отработка замечаний

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -9,11 +9,10 @@ public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
 
     public String get(String key) {
-        String rsl = values.get(key);
-        if (rsl == null) {
+        if (!values.containsKey(key)) {
             throw new IllegalArgumentException(String.format("Not found key %s", key));
         }
-        return rsl;
+        return values.get(key);
     }
 
     private void parse(String[] args) {
@@ -21,23 +20,29 @@ public class ArgsName {
             String tmp = args[i];
             tmp = tmp.substring(1);
             String[] tmpArr = tmp.split("=", 2);
-            if (tmpArr.length < 2 || tmpArr[1].isEmpty()) {
-                throw new IllegalArgumentException(String.format("Error in the line '%s'", tmp));
-            } else {
-                values.put(tmpArr[0], tmpArr[1]);
-            }
+            checkParams(tmpArr);
+            values.put(tmpArr[0], tmpArr[1]);
         }
 
     }
 
+    private void checkParams(String[] tmpArr) {
+        if (tmpArr.length != 2 || tmpArr[1].isEmpty()) {
+            throw new IllegalArgumentException(String.format("Param '%s' is invalid", tmpArr[0]));
+        }
+    }
+
     public static ArgsName of(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Params are not defined");
+        }
         ArgsName names = new ArgsName();
         names.parse(args);
         return names;
     }
 
     public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8", "-Xmx1="});
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8", "-Xmx1=1"});
         System.out.println(jvm.get("Xmx"));
 
         ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
