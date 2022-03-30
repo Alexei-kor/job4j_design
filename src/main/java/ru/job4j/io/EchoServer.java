be32028d92e73ad7e2fbf407f6ecc5487f1091a1
Alexei_korn <aykornjushenkov@gmail.com>
2022-03-30 08:47:51 +0300

    3. Бот [#7921] Отработка замечаний

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -10,7 +10,6 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(15784)) {
-            boolean stop = false;
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream outputStream = socket.getOutputStream()) {
@@ -21,14 +20,11 @@ public class EchoServer {
                     if (str.contains("msg=Hello")) {
                         outputStream.write("Hello".getBytes());
                     } else if (str.contains("msg=Exit")) {
-                        stop = true;
+                        server.close();
                     } else {
                         outputStream.write("What".getBytes());
                     }
                     outputStream.flush();
-                    if (stop) {
-                        server.close();
-                    }
                 }
             }
         } catch (IOException e) {
