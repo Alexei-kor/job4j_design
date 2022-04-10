5f22a8d5e2b65e61c47c67f40c95a18bd21b20c4
Alexei_korn <58764863+Alexei-kor@users.noreply.github.com>
2022-04-10 08:53:07 +0300

    Добавлено удаление записей таблицы

--- Data/models.sql
+++ Data/models.sql
@@ -1,11 +1,12 @@
 
-CREATE TABLE models1 (
+CREATE TABLE models3 (
 	id serial PRIMARY KEY,
 	name TEXT,
 	generation INT,
 	dataBegin text
 );
-INSERT INTO models1(name, generation, databegin) VALUES('жигули', 1, '1976');
-INSERT INTO models1(name, generation, databegin) VALUES('самара', 2, '2004');
-UPDATE models1 SET generation=3;
-SELECT * FROM models1
+INSERT INTO models3(name, generation, databegin) VALUES('жигули', 1, '1976');
+INSERT INTO models3(name, generation, databegin) VALUES('самара', 2, '2004');
+UPDATE models3 SET generation=3;
+SELECT * FROM models3;
+DELETE FROM models3;
