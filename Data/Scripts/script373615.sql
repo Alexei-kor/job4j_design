e2a783cbddf0a9fec41e02dbf9ac11699ec56ed7
Alexei_korn <aykornjushenkov@gmail.com>
2022-04-13 20:50:47 +0300

    0.1. Связи между таблицами [#373615 #287727] Отработка замечаний

--- Data/Scripts/script373615.sql
+++ Data/Scripts/script373615.sql
@@ -1,22 +1,24 @@
 
+/* VINs */
+create table vins(
+	id serial primary key,
+	vin varchar(20)
+);
+
 /* МОДЕЛИ */
 CREATE TABLE models (
 	id serial PRIMARY KEY,
 	name TEXT,
 	generation INT,
-	dataBegin text
+	dataBegin text,
+	vin_id int REFERENCES vins(id)
 );
-INSERT INTO models(name, generation, databegin) VALUES('жигули', 1, '1976');
-INSERT INTO models(name, generation, databegin) VALUES('самара', 2, '2004');
 
 /* ВЛАДЕЛЬЦЫ */
 create TABLE owners(
 	id serial primary key,
 	name varchar(255)
 );
-insert into owners("name") values ('Иванов И И');
-insert into owners("name") values ('Петров П П');
-insert into owners("name") values ('Сидоров С С');
 
 /* ВЛАДЕЛЬЦЫ МАШИН */
 create table autos(
@@ -24,6 +26,15 @@ create table autos(
 	id_m int REFERENCES public.models(id),
 	id_o int references public.owners(id)
 );
+
+INSERT INTO vins(vin) VALUES('54s5dfsfd565dfsdf'), ('548werwerds35df');
+
+INSERT INTO models(name, generation, databegin, vin_id) VALUES('жигули', 1, '1976', 1);
+INSERT INTO models(name, generation, databegin, vin_id) VALUES('самара', 2, '2004', 2);
+
+insert into owners("name") values ('Иванов И И');
+insert into owners("name") values ('Петров П П');
+insert into owners("name") values ('Сидоров С С');
 
 insert into autos(id_m, id_o) values (1, 1);
 insert into autos(id_m, id_o) values (1, 2);
