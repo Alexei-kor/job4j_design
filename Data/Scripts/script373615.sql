088ddb1616fb5cd16b690c3797992edbff90c419
Alexei_korn <aykornjushenkov@gmail.com>
2022-04-14 21:29:02 +0300

    0.1. Связи между таблицами [#373615 #287727] Отработка замечаний

--- Data/Scripts/script373615.sql
+++ Data/Scripts/script373615.sql
@@ -5,13 +5,20 @@ create table vins(
 	vin varchar(20)
 );
 
+/* transmission */
+create table transmission(
+	id serial primary key,
+	name varchar(50)
+);
+
 /* МОДЕЛИ */
 CREATE TABLE models (
 	id serial PRIMARY KEY,
 	name TEXT,
 	generation INT,
 	dataBegin text,
-	vin_id int REFERENCES vins(id)
+	id_tr int REFERENCES transmission(id),
+	vin_id int REFERENCES vins(id) unique
 );
 
 /* ВЛАДЕЛЬЦЫ */
@@ -28,9 +35,10 @@ create table autos(
 );
 
 INSERT INTO vins(vin) VALUES('54s5dfsfd565dfsdf'), ('548werwerds35df');
+INSERT INTO transmission(name) VALUES('механическая'), ('автоматическая');
 
-INSERT INTO models(name, generation, databegin, vin_id) VALUES('жигули', 1, '1976', 1);
-INSERT INTO models(name, generation, databegin, vin_id) VALUES('самара', 2, '2004', 2);
+INSERT INTO models(name, generation, databegin, id_tr, vin_id) VALUES('жигули', 1, '1976', 1, 1);
+INSERT INTO models(name, generation, databegin, id_tr, vin_id) VALUES('самара', 2, '2004', 2, 2);
 
 insert into owners("name") values ('Иванов И И');
 insert into owners("name") values ('Петров П П');
