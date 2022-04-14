f5938bb255d609e1dc5e1c2681735f4dce179770
Alexei_korn <aykornjushenkov@gmail.com>
2022-04-14 21:14:19 +0300

    0. JDBC [#6863] Отработка замечаний CI Bot

--- src/main/java/ru/job4j/jdbc/TestConnection.java
+++ src/main/java/ru/job4j/jdbc/TestConnection.java
@@ -9,7 +9,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class testConnection {
+public class TestConnection {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         checkParams(args);
